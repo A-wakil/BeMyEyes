e5afdc5b30b21eb1baa023c6b39c80360b80a240
A-wakil <abdulwakil.ola@gmail.com>
2025-03-04 12:42:14 -0600

    Initial Commit

---
+++ BeMyEyes/BeMyEyesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeMyEyesApp.swift
+//  BeMyEyes
+//
+//  Created by CAIT on 3/4/25.
+//
+
+import SwiftUI
+
+@main
+struct BeMyEyesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
